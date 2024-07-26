2df72585a2aed630970742073e6c20b64ec424c4
umar-techabout <umartechabout@gmail.com>
2024-07-26 17:51:39 +0500

    fixed issue

--- src/components/News.js
+++ src/components/News.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Timeline from "./timeline/Timeline";
 import BlogDetail from "./blog-detail/BlogDetail";
 import BreadCrumb from "./breadcrumb/BreadCrumb";
