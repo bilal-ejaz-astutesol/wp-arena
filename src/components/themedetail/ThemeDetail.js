9c91b51dec049573ae15adce2e677191fd495ca6
shahidanwarastute <shahidanwarastute@gmail.com>
2024-07-31 10:08:12 +0500

    empty themedetail component added

---
+++ src/components/themedetail/ThemeDetail.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import BreadCrumb from '../breadcrumb/BreadCrumb'
+
+const ThemeDetail = () => {
+  return (
+    <>
+      <BreadCrumb/>
+    </>
+  )
+}
+
+export default ThemeDetail
