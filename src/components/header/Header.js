9c20c312f69afa2daa3f77ee3821039cad149746
shahidanwarastute <shahidanwarastute@gmail.com>
2024-08-01 14:06:58 +0500

    home page responsiveness

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -10,11 +10,11 @@ const Header = () => {
       <header>
         <div className='wpa-header-wrapper wpa-wrapper-sides-spacing wpa-flex wpa-gap-xxl wpa-content-top-bottom-spacing'>
           <div className='wpa-header-logo'>
-            <Link to={"/"}><img src={HeaderLogo} alt="Header Logo"/></Link>
+            <Link to={"/"}><img src={HeaderLogo} alt="Header Logo" /></Link>
           </div>
-          <div className='wpa-flex wpa-space-between wpa-width-100'>
-            <nav className='wpa-parahraph-text wpa-list-style-type-none wpa-anchor-underline-none'>
-              <ul className='wpa-flex wpa-menu-items-gap wpa-regular-font-weight'>
+          <div className='wpa-flex wpa-space-between wpa-center-on-mobile wpa-width-100'>
+            <nav className='wpa-parahraph-text  wpa-list-style-type-none wpa-anchor-underline-none'>
+              <ul className='wpa-flex wpa-desktop-menu wpa-menu-items-gap wpa-regular-font-weight'>
                 <li><Link to="/category/news">News</Link></li>
                 <li><Link to="/category/tutorial">Tutorials</Link></li>
                 <li><Link to="/category/reviews">Reviews</Link></li>
@@ -23,15 +23,39 @@ const Header = () => {
                 <li><Link to="/category/collection">Collection</Link></li>
               </ul>
             </nav>
+                 
             <div className='wpa-right-menu-buttons'>
+            
               <div className='wpa-header-search-bar wpa-flex'>
                 <div className='wpa-flex'>
-                  <input placeholder='Search'  />
-                  <img src={SearchIcon} alt='Search Icon'/>
+                  <input placeholder='Search' />
+                  <img src={SearchIcon} alt='Search Icon' />
                 </div>
                 <button>My Account</button>
               </div>
+              
             </div>
+             {/* mobile menu start */}
+             <div class="menuButton wpa-mobile-menu">
+              <input type="checkbox" id="navcheck" role="button" title="menu" />
+                <label for="navcheck" aria-hidden="true" title="menu">
+                  <span class="burger">
+                    <span class="bar">
+                      <span class="visuallyhidden">Menu</span>
+                    </span>
+                  </span>
+                </label>
+                <ul id="menu" class="menuNav">
+                <li><Link to="/category/news">News</Link></li>
+                <li><Link to="/category/tutorial">Tutorials</Link></li>
+                <li><Link to="/category/reviews">Reviews</Link></li>
+                <li><Link to="/category/comparison">Comparison</Link></li>
+                <li><Link to="/category/resources">Resources</Link></li>
+                <li><Link to="/category/collection">Collection</Link></li>
+                </ul>
+            </div>
+            {/* mobile menu end */}
+         
           </div>
         </div>
       </header>
