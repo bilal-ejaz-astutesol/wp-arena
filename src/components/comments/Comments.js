75d7476581e1f863a0dd6a1f490510e3a7c83944
umar-techabout <umartechabout@gmail.com>
2024-08-01 14:08:08 +0500

    theme detail page by awais

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BrandonKelley from '../../assets/images/brandon-kelley.png'
 import './Comments.css'
-const Comments = () => {
+const Comments = ({data}) => {
+    console.log("comments => ",data)
+
+    const [isDataShow, setisDataShow] = useState(false);
+
     return (
         <>
             <section>
                 <div className='wpa-comments-main wpa-h3-font-size'>
-                    <h3><span>2</span> Comments</h3>
+                    <h3> {isDataShow ? <span>2</span>: null}  {data}</h3>
+
+                    <div class="wpa-right-menu-buttons wpa-content-end wpa-leave-btn">
+                    <button>LEAVE A REPLY</button>
+                    </div>
                     <div className='wpa-comments-common'>
 
                         <div className='wpa-comments-wrapper'>
@@ -15,7 +23,10 @@ const Comments = () => {
                                 <img src={BrandonKelley} alt='Brandon kelley' />
                             </div>
                             <div className='wpa-comments-common-content wpa-h5-font-size wpa-paragraph-text'>
+                                <div className='wpa-flex wpa-space-between ' >
                                 <h5>Brandon kelley</h5>
+                                <h6>27 AUG 2024</h6>
+                                </div>
                                 <p>In the digital era, where online interactions and transactions have become the norm, the importance of identity verification cannot be overstated.  For WordPress site owners, implementing ID verification is a crucial step towards enhancing to using their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
                                 <div className='wpa-button-reply'><button type='button'>Reply</button></div>
 
@@ -26,7 +37,10 @@ const Comments = () => {
                                 <img src={BrandonKelley} alt='Brandon kelley' />
                             </div>
                             <div className='wpa-comments-common-content wpa-h5-font-size wpa-paragraph-text'>
+                                <div className='wpa-flex wpa-space-between'>
                                 <h5>Brandon kelley, <span>Admin</span></h5>
+                                <h6>27 AUG 2024</h6>
+                                </div>
                                 <p>In the digital era, where online interactions and transactions have become the norm, the importance of identity verification cannot be overstated.  For WordPress site owners, implementing ID verification is a crucial step towards enhancing to using their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
                                 <div className='wpa-button-reply'><button type='button'>Reply</button></div>
                             </div>
